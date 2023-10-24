98d65da1fdf03f2ef0cf24a790f7536fa6abd1a6
mightycolony <karthick031995@gmail.com>
2023-10-24 20:36:56 +0530

    new changes

--- firewall_project/rules_adding/views.py
+++ firewall_project/rules_adding/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rules_adding import forms 
 from rules_fetcher_display.models import prerouting,postrouting
 from django.shortcuts import redirect
-
+from .sshcall import SSH 
 # Create your views here.
 def add(request):
 
@@ -18,11 +18,17 @@ def add(request):
           form = forms.preform(request.POST)
 
       if form.is_valid():
+               server_data=[]
                data = form.cleaned_data
                routing=data['routing']
                if routing == "prerouting":   
-                   print(routing)
                    prerouting.objects.get_or_create(**data)
+                   server_data.append(form.cleaned_data['source_ip'])
+                   server_data.append(form.cleaned_data['source_port'])
+                   server_data.form.cleaned_data(['protocol'])
+                   server_data.form.cleaned_data(['destination_ip'])
+                   server_data.form.cleaned_data(['destination_port'])
+                   print(server_data)
                elif routing == "postrouting":
                   print(routing)
                   postrouting.objects.get_or_create(source_ip=data['destination_ip'], destination_ip=data['source_ip'], routing=data['routing'])
@@ -32,3 +38,10 @@ def add(request):
                   postrouting.objects.get_or_create(source_ip=data['destination_ip'], destination_ip=data['source_ip'], routing=data['routing'])
                
    return redirect('rules')
+
+
+
+
+
+connection_call=SSH()
+#connection_call.ssh_call()
